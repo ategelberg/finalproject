e6d57f1eb7ba2bfd23156c6c7d977cfda2a2b903
Anna Tegelberg <ategel@uw.edu>
2018-05-29 20:24:20 -0700

    deleted extra files

--- scripts/coords_cities2.R
+++ scripts/coords_cities2.R
@@ -1,14 +1,3 @@
-# This file is me attempting to match our coordinates to a csv file that I found that has
-# all of the coordinates for most cities. However, when I matched our file with this file 
-# joining, only 4 of the coordinates actaully crossed over. So what I am doing below is 
-# attempting to round up all of the coordinates on both dataframes to whole numbers 
-# so that we can get semi-precise city names for each language. However, each longitude 
-# and latitude column needs to be numeric in order to round the values and some of them right
-# now are strings. All of the stuff I've tried to do is not working to change it to numeric
-# instead of characters so we should discuss what we want to do. Otherwise, if the columns were 
-# numeric we'd be able to merge the dataframes and see if we have enough value matches to use 
-# the rounded coordinates for matching. 
-
 # Load in libraries 
 library(dplyr)
 
@@ -21,18 +10,26 @@ cities <- cities %>%
   select("X", "X.1", "X.2", "X.4", "X.5") %>% 
   rename(Country = X, Region = X.1, City = X.2, Latitude = X.4, Longitude = X.5) 
 
-# This should work to make the cities dataframes lat and long columns numeric 
-# but it's not so....any new solution ideas?
+# Remove first row of df
+cities <- cities[-1, ]
+
+# Make cities Latitude and Longitude columns numeric
 cities <- mutate(cities, Latitude = as.numeric(Latitude))
 cities <- mutate(cities, Longitude = as.numeric(Longitude))
 
-# Round dataframe values 
-cities <- mutate(cities, Latitude = round(Latitude, 1),
-                 Longitude = round(Longitude, 1))
+# Round dataframe values for general city matching
+cities <- mutate(cities, Latitude = round(Latitude, 0),
+                 Longitude = round(Longitude, 0))
 
-extinct_lang <- mutate(extinct_lang, Latitude = round(Latitude,1),
-                       Longitude = round(Longitude, 1))
+extinct_lang <- mutate(extinct_lang, Latitude = round(Latitude, 0),
+                       Longitude = round(Longitude, 0))
 
-# Join dataframes to match latitude and longitude 
-df <- left_join(extinct_lang, cities)
+# Join dataframes to match latitude and longitude per language to a city
+df <- left_join(extinct_lang, cities, by = c('Latitude', 'Longitude'))
+
+languages_cities <- group_by(df, Name.in.English) %>% 
+  filter(row_number()==1)
+
+# Write df to a new csv file in the data folder
+write.csv(languages_cities, file = "../data/languages_cities.csv")
 
